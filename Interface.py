275c31433a2e9bf8b9c891b5c754374fda031a30
Bamgm14 <46135722+Bamgm14@users.noreply.github.com>
2019-07-07 22:38:59 +0530

    Delete Interface.py

--- Interface.py
+++
@@ -1,8 +0,0 @@
-import Sports as s
-Scommand={'?live':s.live,'?status':s.status}
-def SInterface(message):
-    for x in Scommand.keys():
-        if x in message:
-            answer=Scommand[x](str(message.split('!')[1].split('(')[1].split(',')[0]),str(message.split('!')[1].split('(')[1].split(',')[1].split(')')[0]))
-            break
-    return answer
