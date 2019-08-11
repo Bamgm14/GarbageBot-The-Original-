0570688980bc075395bbc4342f06c46fb10209a8
Bamgm14 <46135722+Bamgm14@users.noreply.github.com>
2019-08-11 22:51:56 +0530

    Add files via upload

--- Special Paths/Setup.py
+++ Special Paths/Setup.py
@@ -1,5 +1,5 @@
 import os
-lib=['pycricbuzz','selenium','bs4','urllib3','matplotlib','numpy','requests','gtts','matplotlib','numpy']
+lib=['pycricbuzz','selenium','bs4','urllib3','matplotlib','numpy','requests','gtts','matplotlib','numpy','chatterbot','chatterbot-corpus']
 Execute=['https://youtube-dl.org/downloads/latest/youtube-dl.exe','https://chromedriver.storage.googleapis.com/74.0.3729.6/chromedriver_win32.zip','https://www.7-zip.org/a/7zr.exe','https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-win32.zip',]
 for x in lib:
     os.system('python3 -m pip install '+str(x)+' --user|| python -m pip install '+str(x)+' --user')
