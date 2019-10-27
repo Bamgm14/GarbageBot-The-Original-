c105d0534adf9ce668c5e18ea0e562a649ad5761
Bamgm14 <46135722+Bamgm14@users.noreply.github.com>
2019-10-27 14:48:36 +0530

    Add files via upload

--- Bot.py
+++ Bot.py
@@ -11,7 +11,10 @@ hlp=open('Help.txt','r').read()
 driver = webdriver.Firefox()
 driver.maximize_window()
 driver.get('http://web.whatsapp.com')
+page=driver.page_source
 print('Please Scan the QR Code')
+while page==driver.page_source:
+    pass
 while True:
     try:
         user=u.Users()
@@ -88,6 +91,8 @@ while True:
                         response=str(e)[0].upper()+str(e)[1:]+'\n'
                         textbox.send_keys(response)
                         print (e)
+                driver.get('http://web.whatsapp.com')
             t.sleep(1)
     except Exception as e:
+        driver.get('http://web.whatsapp.com')
         print(e)
